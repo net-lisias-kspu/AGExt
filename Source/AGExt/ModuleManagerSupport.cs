df7634bedc79b7f2c648dba2408928e16a011473
Lisias T <me@lisias.net>
2024-12-11 02:33:09 -0300

    Fixing long due MM support bug.

--- Source/AGExt/ModuleManagerSupport.cs
+++ Source/AGExt/ModuleManagerSupport.cs
@@ -21,7 +21,7 @@ using System.Collections.Generic;
 
 namespace ActionGroupsExtended
 {
-	public static class ModuleManagerSupport
+	public class ModuleManagerSupport : UnityEngine.MonoBehaviour
 	{
 		public static IEnumerable<string> ModuleManagerAddToModList()
 		{
